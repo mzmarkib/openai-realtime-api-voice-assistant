129f05013401de8848a16576d01f55fd1add04dd
mzmarkib <mzmarkib@gmail.com>
2024-10-25 19:55:41 +0530

    Update replit.nix

--- replit.nix
+++ replit.nix
@@ -1,3 +1,18 @@
-{pkgs}: {
-  deps = [ ];
+{ pkgs }: {
+  deps = [
+    pkgs.nodejs-18_x.override {
+      overrides = {
+        nodejs = pkgs.nodejs-18_x;
+      };
+    }
+    # Add Node.js dependencies
+    pkgs.nodePackages.@fastify.formbody
+    pkgs.nodePackages.@fastify.websocket
+    pkgs.nodePackages.axios
+    pkgs.nodePackages.dotenv
+    pkgs.nodePackages.fastify
+    pkgs.nodePackages.form-data
+    pkgs.nodePackages.node-fetch
+    pkgs.nodePackages.ws
+  ];
 }
